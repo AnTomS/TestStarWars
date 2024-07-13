abf26673aec3188ec75ac0d821158b6126de8276
AnTomS <thekeibee@yandex.ru>
2024-07-13 17:52:30 +0800

    Добавлен экран и логика для экрана с планетами. добавлен тулбар

--- app/src/main/java/com/atom/teststarwars/presentation/MainActivity.kt
+++ app/src/main/java/com/atom/teststarwars/presentation/MainActivity.kt
@@ -10,14 +10,17 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.atom.teststarwars.R
 import com.atom.teststarwars.databinding.ActivityMainBinding
 import com.atom.teststarwars.di.AppComponent
+import com.google.android.material.appbar.MaterialToolbar
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
@@ -33,14 +36,23 @@ class MainActivity : AppCompatActivity() {
         (application as App).appComponent.inject(this)
         Log.d("AppComponent", "Injecting MainActivity")
         setupNavigation()
+        setupToolbar()
     }
 
     private fun setupNavigation() {
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         navController = navHostFragment.navController
 
+    }
+
+    private fun setupToolbar() {
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Update the toolbar text
+        val toolbarText = findViewById<TextView>(R.id.toolbar_text)
+        toolbarText.text = "Старые звездные войны"
     }
 
     override fun onSupportNavigateUp(): Boolean {
